6b294e5abae23660a833a5e73909e2ef46d152f6
Lee Inho <inhoking01@gmail.com>
2024-06-16 22:00:14 +0900

    Initial Commit

---
+++ BusanifyUITests/BusanifyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BusanifyUITestsLaunchTests.swift
+//  BusanifyUITests
+//
+//  Created by 이인호 on 6/16/24.
+//
+
+import XCTest
+
+final class BusanifyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
