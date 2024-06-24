5d063b524f8a62fe3113c936dc4651c970845a43
Lee Inho <inhoking01@gmail.com>
2024-06-24 15:27:17 +0900

    feat. Place API 연동

---
+++ Busanify/Domain/Entities/PlaceType.swift
@@ -0,0 +1,15 @@
+//
+//  PlaceType.swift
+//  Busanify
+//
+//  Created by 이인호 on 6/23/24.
+//
+
+import Foundation
+
+enum PlaceType: Int, Codable {
+    case touristAttraction = 76
+    case shopping = 79
+    case accommodation = 80
+    case restaurant = 82
+}
