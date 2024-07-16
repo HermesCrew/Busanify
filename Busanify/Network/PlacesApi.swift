f44f6c706c9e5e3ba23a176c0f7c8c882c7e2ec8
Lee Inho <inhoking01@gmail.com>
2024-07-17 00:54:32 +0900

    chore. PlacesApi에 빠진부분 수정

--- Busanify/Network/PlacesApi.swift
+++ Busanify/Network/PlacesApi.swift
@@ -64,7 +64,7 @@ final class PlacesApi: HomeViewUseCase, PlaceDetailViewUseCase {
                 }
             })
             .decode(type: Place.self, decoder: JSONDecoder())
-            .replaceError(with: Place(id: "", typeId: "", image: "", lat: 0, lng: 0, tel: "", title: "", address: "", openTime: nil, parking: nil, holiday: nil, fee: nil, reservationURL: nil, goodStay: nil, hanok: nil, menu: nil, shopguide: nil, restroom: nil))
+            .replaceError(with: Place(id: "", typeId: "", image: "", lat: 0, lng: 0, tel: "", title: "", address: "", openTime: nil, parking: nil, holiday: nil, fee: nil, reservationURL: nil, goodStay: nil, hanok: nil, menu: nil, shopguide: nil, restroom: nil, isBookmarked: false, avgRating: 0.0))
             .eraseToAnyPublisher()
     }
 }
