66f9ed3f2e0f1811fa87e556200854578ad8a73e
yehjinjang <j95033176@gmail.com>
2024-07-17 21:55:41 +0900

    뷰 연결

--- Busanify/Presentation/Views/Weather/WeatherViewController.swift
+++ Busanify/Presentation/Views/Weather/WeatherViewController.swift
@@ -10,12 +10,66 @@
 // -TODO: 버튼 누르면 날씨 상세 모달뷰 뜨도록
 
 import UIKit
+import WeatherKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, WeatherManagerDelegate {
     private let weatherManager = WeatherManager()
-    
+    private let weatherLabel = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setupNavigationBar()
+        weatherManager.delegate = self
         weatherManager.startFetchingWeather()
+    }
+    
+    private func setupNavigationBar() {
+        self.title = "날씨"
+        self.navigationController?.navigationBar.barTintColor = .gray
+        
+        let backButton = UIButton(type: .system)
+        backButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        backButton.setTitle("back", for: .normal)
+        backButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
+        backButton.sizeToFit()
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        
+        let backBarButtonItem = UIBarButtonItem(customView: backButton)
+        self.navigationItem.leftBarButtonItem = backBarButtonItem
+    }
+    
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        weatherLabel.translatesAutoresizingMaskIntoConstraints = false
+        weatherLabel.textAlignment = .center
+        weatherLabel.numberOfLines = 0
+        
+        view.addSubview(weatherLabel)
+        
+        NSLayoutConstraint.activate([
+            weatherLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            weatherLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            weatherLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            weatherLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    func didUpdateWeather(_ weather: Weather) {
+        DispatchQueue.main.async {
+            let temperature = weather.currentWeather.temperature.value
+            self.weatherLabel.text = "Temperature: \(temperature)°C"
+        }
+    }
+    
+    func didFailWithError(_ error: Error) {
+        DispatchQueue.main.async {
+            self.weatherLabel.text = "Failed to get weather: \(error.localizedDescription)"
+        }
     }
 }
