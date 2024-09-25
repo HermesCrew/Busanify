5a25e410d0d7ed26f6b6f935cc1b3c63ac2a0e18
seokyung402 <wni402@naver.com>
2024-09-25 20:19:13 +0900

    Feat. 글 등록 시 토스트메시지 띄우기

--- Busanify/Presentation/Views/Community/CommunityViewController.swift
+++ Busanify/Presentation/Views/Community/CommunityViewController.swift
@@ -39,7 +39,7 @@ class CommunityViewController: UIViewController  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureUI()
         bind()
         
@@ -88,6 +88,28 @@ class CommunityViewController: UIViewController  {
                 self?.tableView.reloadData()
             }
             .store(in: &cancellables)
+    }
+    
+    func showToast(_ view: UIView, message: String, duration: TimeInterval = 2.0) {
+        let toastLabel = UILabel(frame: CGRect(x: view.frame.size.width/2 - 150, y: view.frame.size.height-150, width: 300, height: 40))
+        toastLabel.backgroundColor = .gray
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = NSTextAlignment.center;
+        view.addSubview(toastLabel)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        
+        UIView.animate(withDuration: 0.4,
+                       delay: duration - 0.4,
+                       options: UIView.AnimationOptions.curveEaseOut,
+                       animations: {
+            toastLabel.alpha = 0.0
+        },
+                       completion: { (finished) in
+            toastLabel.removeFromSuperview()
+        })
     }
 }
 
@@ -183,6 +205,10 @@ extension CommunityViewController: CommunityTableViewCellDelegate {
 }
 
 extension CommunityViewController: AddPostViewControllerDelegate {
+    func showToastMessage(_ message: String) {
+        showToast(view, message: message)
+    }
+    
     func didCreatePost() {
         postViewModel.fetchPosts()
     }
