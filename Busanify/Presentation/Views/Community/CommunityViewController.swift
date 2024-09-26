31a6b8a92ac0bac51f025ad1ed12e40195135bb9
seokyung402 <wni402@naver.com>
2024-09-26 20:35:09 +0900

    refactor. showToast 삭제

--- Busanify/Presentation/Views/Community/CommunityViewController.swift
+++ Busanify/Presentation/Views/Community/CommunityViewController.swift
@@ -156,28 +156,6 @@ class CommunityViewController: UIViewController  {
             }
             .store(in: &cancellables)
     }
-    
-    func showToast(_ view: UIView, message: String, duration: TimeInterval = 2.0) {
-        let toastLabel = UILabel(frame: CGRect(x: view.frame.size.width/2 - 150, y: view.frame.size.height-150, width: 300, height: 40))
-        toastLabel.backgroundColor = .gray
-        toastLabel.textColor = UIColor.white
-        toastLabel.textAlignment = NSTextAlignment.center;
-        view.addSubview(toastLabel)
-        toastLabel.text = message
-        toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 10;
-        toastLabel.clipsToBounds  =  true
-        
-        UIView.animate(withDuration: 0.4,
-                       delay: duration - 0.4,
-                       options: UIView.AnimationOptions.curveEaseOut,
-                       animations: {
-            toastLabel.alpha = 0.0
-        },
-                       completion: { (finished) in
-            toastLabel.removeFromSuperview()
-        })
-    }
 }
 
 extension CommunityViewController: UITableViewDelegate, UITableViewDataSource {
