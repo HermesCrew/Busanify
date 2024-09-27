e2d95c3f672a63907bf53b4751aac6be946d5328
Lee Inho <inhoking01@gmail.com>
2024-09-27 21:26:30 +0900

    chore. 프로필이미지 선택 안했을때 기본 이미지 추가

--- Busanify/Presentation/Views/Community/Post/PostDetailViewController.swift
+++ Busanify/Presentation/Views/Community/Post/PostDetailViewController.swift
@@ -37,6 +37,7 @@ class PostDetailViewController: UIViewController {
     
     private lazy var profileImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.crop.circle")
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 15
